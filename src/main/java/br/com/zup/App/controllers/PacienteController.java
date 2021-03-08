a52376dfbe6dd3cda35c3dad77dc0f7d10009b42
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-08 14:48:07 -0300

    Adicionar caminho para pesquisa de paciente pelo cpf

--- src/main/java/br/com/zup/App/controllers/PacienteController.java
+++ src/main/java/br/com/zup/App/controllers/PacienteController.java
@@ -5,6 +5,7 @@ import br.com.zup.App.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("pacites/")
@@ -18,5 +19,15 @@ public class PacienteController {
     public Paciente cadastrarPaciente(@RequestBody Paciente paciente){
         Paciente objetoPaciente = pacienteService.salvarPaciente(paciente);
         return paciente;
+    }
+
+    @GetMapping("{cpf}/")
+    public Paciente pesquisarPaciente(@PathVariable String cpf){
+        try{
+            Paciente paciente = pacienteService.pesquisarPacientePeloCPF(cpf);
+            return paciente;
+        }catch (RuntimeException error){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, error.getMessage());
+        }
     }
 }
