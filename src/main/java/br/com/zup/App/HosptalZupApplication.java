55b95afae1fed204a9797ad2ef12ef934367aaa0
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-08 13:56:11 -0300

    Criar projeto de gerenciamento de pacientes

---
+++ src/main/java/br/com/zup/App/HosptalZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosptalZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosptalZupApplication.class, args);
+	}
+
+}
