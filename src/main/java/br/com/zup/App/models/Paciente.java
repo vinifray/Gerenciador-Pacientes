f5ae7c084f5b792f8714c4f93aa8a22d5e752847
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-08 14:08:22 -0300

    Adicionar modelo de paciente

---
+++ src/main/java/br/com/zup/App/models/Paciente.java
@@ -0,0 +1,45 @@
+package br.com.zup.App.models;
+
+import java.util.List;
+
+public class Paciente {
+    private String nome;
+    private int idade;
+    private String cpf;
+    private List<Historico> historico;
+
+    public Paciente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Historico> getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(List<Historico> historico) {
+        this.historico = historico;
+    }
+}
