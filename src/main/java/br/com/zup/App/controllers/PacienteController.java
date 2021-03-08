4aab932b2250a941c350665f30f11fe5f639c923
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-08 14:51:47 -0300

    Corrigir endpoint do pacientes

--- src/main/java/br/com/zup/App/controllers/PacienteController.java
+++ src/main/java/br/com/zup/App/controllers/PacienteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("pacites/")
+@RequestMapping("pacientes/")
 public class PacienteController {
 
     @Autowired
